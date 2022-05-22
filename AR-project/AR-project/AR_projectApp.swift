03091d01bf23db85329f384159cf7da9645ff8cd
Renyi-cmd <60345741+Renyi-cmd@users.noreply.github.com>
2022-05-22 13:26:08 +0800

    创建项目

---
+++ AR-project/AR-project/AR_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_projectApp.swift
+//  AR-project
+//
+//  Created by everyone on 2022/5/22.
+//
+
+import SwiftUI
+
+@main
+struct AR_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
